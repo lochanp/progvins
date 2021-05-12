70337a4a69bf261547395bf985bc0147e72b73ce
Lochan Potdar <lochan.potdar1998@gmail.com>
2021-05-12 17:38:31 +0530

    ready for demo

--- app.js
+++ app.js
@@ -96,6 +96,6 @@ let port = process.env.PORT;
 if(port === null || port === "") {
     port = 8000;
 }   
-app.listen(port,() => {
+app.listen(3000,() => {
     console.log(`Server Started on port ${port}`)
 })
