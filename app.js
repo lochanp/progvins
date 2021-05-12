8288d745f37f383aaef545a81a1b139c1cc81278
Lochan Potdar <lochan.potdar1998@gmail.com>
2021-05-12 13:42:02 +0530

    files added

--- app.js
+++ app.js
@@ -96,6 +96,6 @@ let port = process.env.PORT;
 if(port === null || port === "") {
     port = 8000;
 }   
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log(`Server Started on port ${port}`)
 })
